69ec050596234d65a070968f45c6ac41f631d50e
ruben <r.j.vanduuren@student.tudelft.nl>
2024-11-27 16:48:08 +0100

    Small changes

---
+++ tests/test_utils.py
@@ -0,0 +1,24 @@
+from project8.neural_network import utils
+import torch
+
+
+def count_points_on_planes(points: torch.Tensor, tolerance: float = 1e-6) -> list:
+    counts = []
+    for i in range(points.shape[1]):
+        counts.append(torch.sum(torch.abs(points[:, points.shape[1] - i - 1]) <= tolerance).item())
+    return counts
+
+
+def test_parameter_space():
+    device = utils.get_device('cpu')
+    domain = [[0, 1], [0, 1], [0, 1]]
+    n = 10000
+    space = utils.ParameterSpace(domain, device)
+    bound_sel = torch.tensor([[1, 1], [1, 1], [0, 0]], device=device)
+    tmr = utils.timer(); tmr.start()
+    loc = space.select_bndry_rand(n, bound_sel)
+    tmr.stop(); tmr.read()
+    assert (count_points_on_planes(loc)[0] == 0
+            and count_points_on_planes(loc)[1] > 0
+            and count_points_on_planes(loc)[2] > 0
+            and tmr.t_elapsed < 0.3)
