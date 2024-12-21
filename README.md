3a448585dc455ccd93bffb02d5ac86877d56b9bc
coenvdvelde <127772417+coenvdvelde@users.noreply.github.com>
2024-12-21 20:06:28 +0100

    Revert "Merge branch 'coenvdvelde-MatrixGenerator-for-N_dimensional-Laplacian' of https://github.com/Project-08/Project-8 into coenvdvelde-MatrixGenerator-for-N_dimensional-Laplacian"
    This reverts commit 3c19810bb949e399e64d8b1224b643bf51561c77, reversing
    changes made to 95b106148e8de5057dbbb699a3aa2e972c0ec86d.

--- README.md
+++ README.md
@@ -2,38 +2,14 @@
 Neural Networks for Partial Differential Equations
 
 ## Installing & Running
-To install the project8 package, along with development dependencies, install the [CUDA Toolkit](https://developer.nvidia.com/cuda-toolkit), [AMGX](https://github.com/NVIDIA/AMGX), and [pyamgx](https://github.com/shwina/pyamgx).
-
-Then, run
+To install the project8 package, along with development dependencies, run
 ```sh
 pip install '.[dev]'
 ```
 
 To run the project as an executable, run
 ```sh
-python -m project8
-```
-
-## Using Docker
-On Windows and MacOS, it's likely best to do everything in the docker container. Either download the docker image from the github repository's container repository, and tag it as `project8`
-```sh
-docker pull ghcr.io/project-08/project-8/project8:latest
-```
-
-Or build it as follows
-```sh
-cd docker
-docker image build -t project8 .
-```
-
-Then to run an arbitrary command in docker, run
-```sh
-./docker_run.sh <YOUR_COMMAND>
-```
-
-To open an interactive shell in docker, simply run
-```sh
-./docker_run.sh bash
+python project8
 ```
 
 ## Way of Working
--- project8/__main__.py
+++ project8/__main__.py
@@ -1,63 +1,6 @@
-import argparse
-import gc
-import logging
-
-import pyamgx
-
-from project8.bench.sparse import bench_solve_tri
-from project8.sparse import (AMGX_CONFIGS, AmgXSolve, CuPySolve,
-                             CuSPARSETriSolve, TriSolve)
-
-
 def main() -> None:
-    logging.basicConfig(level=logging.INFO)
-
-    parser = argparse.ArgumentParser(prog='project8')
-    commands = parser.add_subparsers(dest="command")
-
-    bench = commands.add_parser("bench")
-    benchmark = bench.add_subparsers(dest="benchmark")
-    solve_tri = benchmark.add_parser("solve-tri")
-
-    solve_tri.add_argument("--min", type=int, default=10)
-    solve_tri.add_argument("--max", type=int, default=1000)
-    solve_tri.add_argument("--steps", type=int, default=10)
-    solve_tri.add_argument("--attempts", type=int, default=10)
-    solve_tri.add_argument(
-        "--solvers",
-        nargs='+',
-        required=True,
-    )
-    solve_tri.add_argument("--plot", action='store_true', default=False)
-
-    args = parser.parse_args()
-
-    if args.command == "bench":
-        if args.benchmark == "solve-tri":
-            solvers: list[TriSolve] = []
-            for solver in args.solvers:
-                match solver:
-                    case "cuSPARSE":
-                        solvers.append(CuSPARSETriSolve())
-                    case "CuPy":
-                        solvers.append(CuPySolve())
-                    case "AmgX":
-                        solvers.extend(
-                            (AmgXSolve(config) for config in AMGX_CONFIGS))
-                    case _ if solver in AMGX_CONFIGS:
-                        solvers.append(AmgXSolve(solver))
-                    case _:
-                        raise ValueError(f"Unrecognized solver: {solver}")
-
-            bench_solve_tri(range(args.min, args.max,
-                                  (args.max - args.min) // args.steps),
-                            solvers=solvers,
-                            attempts=args.attempts,
-                            plot=args.plot)
-            del solvers
-
-    gc.collect()
-    pyamgx.finalize()
+    # Main function is called when project8 is run as a standalone program
+    pass
 
 
 if __name__ == "__main__":
