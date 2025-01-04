6f7a8a9a791d9b9ca6413a65d3c560182364353e
coenvdvelde <127772417+coenvdvelde@users.noreply.github.com>
2025-01-04 19:43:34 +0100

    implemented actual snake_case

--- project8/snake_case.py
+++ project8/snake_case.py
@@ -6,15 +6,15 @@ from multiprocessing import Pool
 from typing import List, Any, Callable
 
 
-def FDLaplacian_9_point_ND(steps: int,
-                           stepsize: float,
-                           dimensions: int) -> Any:
+def fd_laplacian_9_point_nd(steps: int,
+                            stepsize: float,
+                            dimensions: int) -> Any:
     diagonal = np.ones(steps)/stepsize
     offsets: List[int] = [0, -1]
-    D = sp.diags([diagonal, -diagonal],
+    d = sp.diags([diagonal, -diagonal],
                  offsets=offsets,
                  shape=(steps, steps-1))
-    L = D.T@D
+    L = d.T@d
 
     sub_matrices = list()
     for dim in range(dimensions):
@@ -24,7 +24,7 @@ def FDLaplacian_9_point_ND(steps: int,
     return np.sum(sub_matrices)
 
 
-def FDLaplacian_13_point_ND(steps: int, stepsize: float) -> Any:
+def fd_laplacian_13_point_nd(steps: int, stepsize: float) -> Any:
     diagonal1 = np.ones(steps-1)/(stepsize**2)*2.5
     diagonal2 = np.ones(steps-2)/(stepsize**2)*-(4/3)
     diagonal3 = np.ones(steps-3)/(stepsize**2)*(1/12)
@@ -49,11 +49,11 @@ def _source_func(x: float, y: float, z: float) -> np.float64:
     return np.float64(-np.pi*np.pi*x*y*np.sin(np.pi*z))
 
 
-def _Get_index(x: int, y: int, z: int, steps: int) -> int:
+def _get_index(x: int, y: int, z: int, steps: int) -> int:
     return (x-1)+(steps-1)*(y-1)+(steps-1)*(steps-1)*(z-1)
 
 
-def _Get_9_point_source_func(
+def _get_9_point_source_func(
         func: Callable[[float, float, float], np.float64],
         steps: int,
         stepsize: float
@@ -62,19 +62,19 @@ def _Get_9_point_source_func(
     for z in range(1, steps):
         for y in range(1, steps):
             for x in range(1, steps):
-                result[Get_index(x, y, z, steps)] =\
+                result[_get_index(x, y, z, steps)] =\
                     func(x*stepsize, y*stepsize, z*stepsize)
                 # Boundary Condition:
                 if (y == (steps-1)):
-                    result[Get_index(x, y, z, steps)]\
+                    result[_get_index(x, y, z, steps)]\
                         += x*stepsize*np.sin(np.pi*z*stepsize)/(stepsize**2)
                 if (x == (steps-1)):
-                    result[Get_index(x, y, z, steps)]\
+                    result[_get_index(x, y, z, steps)]\
                         += y*stepsize*np.sin(np.pi*z*stepsize)/(stepsize**2)
     return result
 
 
-def _Get_13_point_source_func(
+def _get_13_point_source_func(
         func: Callable[[float, float, float], np.float64],
         steps: int,
         stepsize: float
@@ -83,53 +83,53 @@ def _Get_13_point_source_func(
     for z in range(1, steps):
         for y in range(1, steps):
             for x in range(1, steps):
-                result[Get_index(x, y, z, steps)] =\
+                result[_get_index(x, y, z, steps)] =\
                     func(x*stepsize,
                          y*stepsize,
                          z*stepsize
                          )
                 # Boundary Condition:
                 if (y == (steps-2)):
-                    result[Get_index(x, y, z, steps)]\
+                    result[_get_index(x, y, z, steps)]\
                         -= (1/12)*x*stepsize*np.sin(np.pi*z*stepsize)\
                         / (stepsize**2)
                 if (y == (steps-1)):
-                    result[Get_index(x, y, z, steps)]\
+                    result[_get_index(x, y, z, steps)]\
                         += (4/3-0.16)*x*stepsize*np.sin(np.pi*z*stepsize)\
                         / (stepsize**2)\
                         + (1/12) * func(x*stepsize,
                                         (y+1)*stepsize,
                                         z*stepsize)
                 if (y == 1):
-                    result[Get_index(x, y, z, steps)]\
+                    result[_get_index(x, y, z, steps)]\
                         += (1/12) * func(x*stepsize,
                                          (y-1)*stepsize,
                                          z*stepsize)
 
                 if (x == (steps-2)):
-                    result[Get_index(x, y, z, steps)]\
+                    result[_get_index(x, y, z, steps)]\
                         -= (1/12)*y*stepsize*np.sin(np.pi*z*stepsize)\
                         / (stepsize**2)
                 if (x == (steps-1)):
-                    result[Get_index(x, y, z, steps)]\
+                    result[_get_index(x, y, z, steps)]\
                         + (4/3-0.16)*y*stepsize*np.sin(np.pi*z*stepsize)\
                         / (stepsize**2)\
                         + (1/12) * func((x+1)*stepsize,
                                         y*stepsize,
                                         z*stepsize)
                 if (x == 1):
-                    result[Get_index(x, y, z, steps)]\
+                    result[_get_index(x, y, z, steps)]\
                         += (1/12) * func((x-1)*stepsize,
                                          y*stepsize,
                                          z*stepsize)
 
                 if (z == (steps-1)):
-                    result[Get_index(x, y, z, steps)]\
+                    result[_get_index(x, y, z, steps)]\
                         += (1/12) * func(x*stepsize,
                                          y*stepsize,
                                          (z+1)*stepsize)
                 if (z == 1):
-                    result[Get_index(x, y, z, steps)]\
+                    result[_get_index(x, y, z, steps)]\
                         += (1/12) * func(x*stepsize,
                                          y*stepsize,
                                          (z-1)*stepsize)
@@ -137,7 +137,7 @@ def _Get_13_point_source_func(
     return result
 
 
-def _Get_3D_analytical(
+def _get_3d_analytical(
         steps: int,
         stepsize: float
         ) -> np.ndarray[tuple[int], np.dtype[np.float64]]:
@@ -146,51 +146,45 @@ def _Get_3D_analytical(
     for z in range(1, steps):
         for y in range(1, steps):
             for x in range(1, steps):
-                result[Get_index(x, y, z, steps)] =\
+                result[_get_index(x, y, z, steps)] =\
                     stepsize*stepsize*x*y*np.sin(np.pi*z*stepsize)
     return result
 
 
-def _Get_error_9(size: int) -> np.float64:
+def _get_error_9(size: int) -> np.float64:
     n = size
     u_num = la.spsolve(
-        FDLaplacian_9_point_ND(n, 1/n, 3),
-        Get_9_point_source_func(_source_func, n, 1/n)
+        fd_laplacian_9_point_nd(n, 1/n, 3),
+        _get_9_point_source_func(_source_func, n, 1/n)
         )
-    u_exact = Get_3D_analytical(n, 1/n)
+    u_exact = _get_3d_analytical(n, 1/n)
     squares = (u_num-u_exact)**2
     mean = np.mean(squares)
     root = np.sqrt(mean)
     return np.float64(root)
 
 
-def _Get_error_13(size: int) -> np.float64:
+def _get_error_13(size: int) -> np.float64:
     n = size
     u_num = la.spsolve(
-        FDLaplacian_13_point_ND(n, 1/n),
-        Get_13_point_source_func(_source_func, n, 1/n)
+        fd_laplacian_13_point_nd(n, 1/n),
+        _get_13_point_source_func(_source_func, n, 1/n)
         )
-    u_exact = Get_3D_analytical(n, 1/n)
+    u_exact = _get_3d_analytical(n, 1/n)
     squares = (u_num-u_exact)**2
     mean = np.mean(squares)
     root = np.sqrt(mean)
     return np.float64(root)
 
 
-def _Generate_plot() -> None:
+def _generate_plot() -> None:
     steps_array = np.array([4])
     with Pool() as pool:
-        RMS_13 = pool.map(Get_error_13, steps_array)
+        RMS_13 = pool.map(_get_error_13, steps_array)
     with Pool() as pool:
-        RMS_9 = pool.map(Get_error_9, steps_array)
+        RMS_9 = pool.map(_get_error_9, steps_array)
     plt.semilogy(steps_array, RMS_9, label="RMS_9")
     plt.semilogy(steps_array, RMS_13, label="RMS_13")
     plt.legend()
     plt.show()
     plt.savefig("3D Error until N=50")
-
-
-# if __name__ == "__main__":
-    # Generate_plot()
-    # Get_error_9(4)
-    # Get_error_13(4)
